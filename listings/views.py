fb8929487493e24caa4cd3eb9f760bdebc1cb0b7
Farid Ansari <farid12ansari7@gmail.com>
2019-08-15 00:32:28 +0530

    fixed listing url and more listing

--- listings/views.py
+++ listings/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html', {
-        'name': 'Farid'
-    })
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
